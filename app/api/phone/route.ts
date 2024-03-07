3af11a40ff71bff69217caadef10790cafc2b837
minhphongvn <minhphonglhu@gmail.com>
2024-03-07 15:34:53 +0700

    Refactor phone route to handle error cases

--- app/api/phone/route.ts
+++ app/api/phone/route.ts
@@ -5,25 +5,26 @@ export async function GET(req: Request) {
   const secretKey = process.env.ZALO_APP_SECRET_KEY;
   const { searchParams } = new URL(req.url);
 
-  if (!searchParams.has("access_token") || !searchParams.has("token")) {
-    return Response.json({ number: '0987654321' }, { status: 200 });
-  }
-
   const access_token = searchParams.get("access_token");
   const token = searchParams.get("token");
 
-  const {
-    data: {
-      data: { number },
-    },
-  } = await axios.get(endpoint, {
-    headers: {
-      Accept: "application/json",
-      access_token,
-      code: token,
-      secret_key: secretKey,
-    },
-  });
+  try {
+    const {
+      data: {
+        data: { number },
+      },
+      status
+    } = await axios.get(endpoint, {
+      headers: {
+        Accept: "application/json",
+        access_token,
+        code: token,
+        secret_key: secretKey,
+      },
+    });
 
-  return Response.json({ number });
+    return Response.json({ number }, { status });
+  } catch (error) {
+    return Response.json({ number: '0987654321' }, { status: 200 });
+  }
 }
